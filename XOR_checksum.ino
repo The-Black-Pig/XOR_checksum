d607e146acf4627a0b56f6d426ab48fb86b45e0d
Steve Clements <30567784+The-Black-Pig@users.noreply.github.com>
2019-06-10 16:26:18 +0200

    Add files via upload

---
+++ XOR_checksum.ino
@@ -0,0 +1,24 @@
+// calculates the checksum of any given ASCII string using XOR
+// sketch by Steve Clements 2019
+
+char myArray[] = "arduino uno";
+byte xorTemp;
+
+void setup() {
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // print first character ASCII decimal value
+  xorTemp = byte(myArray[0]);
+  // process the remaining string characters
+  Serial.println(xorTemp);
+  for(int i = 1; i < sizeof(myArray) - 1; i++){
+    xorTemp ^= byte(myArray[i]);
+    Serial.println(xorTemp);
+    delay(250);
+    }
+  Serial.print("The checksum equals ");Serial.println(xorTemp);
+
+}
